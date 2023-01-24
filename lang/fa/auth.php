67cc1b6d062e959afeb39e0ae489a9a537b01dc4
tinar <shamsamir3333@gmail@gmail.com>
2023-01-24 15:12:55 +0330

    add project to git

---
+++ lang/fa/auth.php
@@ -0,0 +1,22 @@
+<?php 
+
+    return 
+    [
+        "register user" => "ثبت نام حساب کاربری", 
+        "email" => "ایمیل", 
+        "password" => "رمز عبور", 
+        "login" => "ورود", 
+        "name" => "نام",
+        "register" => "ثبت نام",
+        "confirm_passwrod" => "تایید رمزعبور", 
+        "forget_password" => "فراموشی رمزعبور",
+        "reset_password" => "رمزعبور خود را فراموش کرده اید؟", 
+        "remember me" => "مرا به خاطر بسپار!", 
+        "logout" => "خروج",
+        "not a member" => "حساب کاربری ندارید? ",
+        "Do you have an account?" => "حساب کاربری دارید؟",
+        "input_email" => "ایمیل خود را وارد کنید", 
+        "input_password" => "رمزعبور خود را وارد کنید",
+        "input_confirm_password" => "رمزعبور خود را تایید کنید", 
+        "phone_number" => "شماره تلفن خود را وارد کنید"
+    ];
